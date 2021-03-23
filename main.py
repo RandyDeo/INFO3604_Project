a05479230b3f5bee313f340f053f174c2938e2a4
RandyDeo <54963299+RandyDeo@users.noreply.github.com>
2021-03-23 08:42:12 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import timedelta
 
-from models import db, User, Student
+import os
+
+from models import db, User, Student, Business, Internship, FileContents
 
 ''' Begin boilerplate code '''
 
@@ -71,7 +73,7 @@ def signup():
         email = request.form.get('email')
         name = request.form.get('name')
         occupation = request.form.get('occupation')
-        password = request.form.get('password')
+        password = request.form.get('pass')
         user = User.query.filter_by(email=email).first()
         if user is None:
             new_user = User(name=name, email=email, occupation=occupation)
@@ -80,7 +82,7 @@ def signup():
                 db.session.add(new_user)
                 db.session.commit()
                 # login_user(new_user)
-                #return render_template("student-homepage.html"), 201
+                # return render_template("student-homepage.html"), 201
                 return render_template('Login.html'), 201
             except IntegrityError:
                 db.session.rollback()
@@ -93,80 +95,168 @@ def signup():
 # new_user.password = generate_password_hash(password, method='sha256')
 
 # add the new user to the database
-#COMMENTING THIS OUT BECAUSE YEAH NO
-#@app.route("/login", methods=(['GET', 'POST']))
-#def login():
-    #if request.method == 'GET':
-        #return render_template('login.html')
+# COMMENTING THIS OUT BECAUSE YEAH NO
+# @app.route("/login", methods=(['GET', 'POST']))
+# def login():
+# if request.method == 'GET':
+# return render_template('login.html')
 
-    #elif request.method == 'POST':
-        #email = request.form.get('email')
-        #password = request.form.get('password')
+# elif request.method == 'POST':
+# email = request.form.get('email')
+# password = request.form.get('password')
 
-        #if not (current_user == authenticate(email, password)):
-            #pass
-        #else:
-            #return render_template('student-homepage')
+# if not (current_user == authenticate(email, password)):
+# pass
+# else:
+# return render_template('student-homepage')
 
-        #user = User.query.filter_by(email=email).first()
-        #if user and user.check_password_hash(password):
-            #try:
-                #login_user(user, remember=True)  # this is an error but line has to be there
-                #return render_template('student-homepage.html'), 200
-            #except IntegrityError:
-                #return 'Email address does not exist', render_template("signup.html"), 400
-    #return render_template('student-homepage.html')
+# user = User.query.filter_by(email=email).first()
+# if user and user.check_password_hash(password):
+# try:
+# login_user(user, remember=True)  # this is an error but line has to be there
+# return render_template('student-homepage.html'), 200
+# except IntegrityError:
+# return 'Email address does not exist', render_template("signup.html"), 400
+# return render_template('student-homepage.html')
 
-@app.route ("/login", methods =(['GET', 'POST']))
+@app.route("/login", methods=(['GET', 'POST']))
 def login():
     if request.method == 'GET':
         return render_template('login.html')
 
     elif request.method == 'POST':
         email = request.form.get('email')
-        password = request.form.get('password')
+        password = request.form.get('pass')
 
         user = User.query.filter_by(email=email).first()
         if user and user.check_password(password):
             time = timedelta(hours=1)
             login_user(user, False, time)
-            return studentHome(), 200
+            if user.occupation == "Business" :
+                return businessHome(), 200
+            if user.occupation == "Student":
+                return studentHome(), 200
+            elif user.occupation == "DCIT":
+                return dcitHome(), 200
         if user is None:
             return "Please create an account!"
-            #return render_template('signup.html'), 401
+            # return render_template('signup.html'), 401
         return "Invalid login", 401
 
-    #return render_template("student-homepage.html")
+    # return render_template("student-homepage.html")
+
 
 @app.route("/studentHome", methods=(['GET']))
 @login_required
 def studentHome():
     return render_template("student-homepage.html")
 
+
 @app.route("/businessHome", methods=(['GET']))
-#@login_required
+# @login_required
 def businessHome():
     return render_template("business-homepage.html")
+
+
+@app.route("/dcitHome", methods=(['GET']))
+@login_required
+def dcitHome():
+    return render_template("DCIT-homepage.html")
+
 
 @app.route("/studentRegistration")
 def studentRegistration():
     return render_template("student-registration.html")
 
-@app.route ("/displayStudentForm")
+
+@app.route("/displayStudentForm", methods=(['GET', 'POST']))
 def displayStudentForm():
-    return render_template("student-registration-form.html")
+    if request.method == 'GET':
+        return render_template('student-registration-form.html')
+
+    elif request.method == 'POST':
+        name = request.form.get('fname')
+        email = request.form.get('emails')
+        uwiid = request.form.get('uwiid')
+        country = request.form.get('country')
+        curr_degree = request.form.get('curr_degree')
+        year_of_study = request.form.get('yos')
+        credit = request.form.get('credits')
+        enrollment = request.form.get('enrollment')
+
+        transcript = request.files['transcript']
+        resume = request.files['resume']
+        essay = request.files['essay']
+
+        student = Student.query.filter_by(uwiid=uwiid).first()
+
+        transcript.save(os.path.join("uploads", uwiid + "_transcript.pdf"))
+        resume.save(os.path.join("uploads", uwiid + "_resume.pdf"))
+        essay.save(os.path.join("uploads", uwiid + "_essay.pdf"))
+
+        if student is None and transcript.filename != '' and resume.filename != '' and essay.filename != '':
+            new_student = Student(name=name, email=email, uwiid=uwiid, country=country,
+                                  year_of_study=year_of_study, credits=credit, enrollment=enrollment,
+                                  curr_degree=curr_degree, transcript=(uwiid + "_transcript.pdf"),
+                                  resume=(uwiid + "_resume.pdf"), essay=(uwiid + "_essay.pdf"))
+            try:
+                db.session.add(new_student)
+                db.session.commit()
+                return render_template('student-homepage.html')
+            except IntegrityError:
+                return 'Application does not exist', render_template("student-registration.html"), 400
+        return
+
 
 @app.route("/businessRegistration")
 def businessRegistration():
     return render_template("business-registration.html")
 
-@app.route ("/displayBusinessForm")
+
+@app.route("/displayBusinessForm", methods=(['GET', 'POST']))
 def displayBusinessForm():
-    return render_template("business-registration-form.html")
+    if request.method == 'GET':
+        return render_template('business-registration-form.html')
+
+    elif request.method == 'POST':
+        bname = request.form.get('bname')
+        num_interns = request.form.get('num_interns')
+        duration = request.form.get('duration')
+        credits = request.form.get('credits')
+        stipend = request.form.get('stipend')
+        tech_skills = request.form.get('tech_skills')
+        soft_skills = request.form.get('soft_skills')
+
+        business = Business.query.filter_by(bname=bname).first()
+
+        if business is None:
+            new_business = Business(bname=bname, num_interns=num_interns, duration=duration, stipend=stipend,
+                                    credits=credits, tech_skills=tech_skills, soft_skills=soft_skills)
+
+            db.session.add(new_business)
+            db.session.commit()
+
+            proj_name = request.form.get('proj_name')
+            proj_descript = request.form.get('proj_descript')
+            activities = request.form.get('activities')
+            internship = Internship.query.filter_by(proj_name=proj_name).first()
+
+            if internship is None:
+                new_internship = Internship(proj_name=proj_name, proj_descript=proj_descript, activities=activities,
+                                            business_ID=new_business.businessID)
+            try:
+                db.session.add(new_internship)
+                db.session.commit()
+                return render_template('business-homepage.html')
+            except IntegrityError:
+                return 'Application does not exist', render_template("business-registration.html"), 400
+    return
+
 
 @login_manager.unauthorized_handler
 def unauthorized():
     return render_template('signup.html')
+
 
 @app.route("/logout", methods=(['GET']))
 @login_required
@@ -184,4 +274,5 @@ def logout():
 
 # return 'Saved ' + file.filename + ' to the database!'
 
-#@app.route("/register", methods=(['POST']))
+
+# @app.route("/register", methods=(['POST']))
--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-
 # Model for User
 class User(db.Model, UserMixin, ):
     id = db.Column(db.Integer, primary_key=True)
@@ -43,22 +42,23 @@ class Student(db.Model, UserMixin, ):
     studentID = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    UWI_ID = db.Column(db.Integer, nullable=False)
+    uwiid = db.Column(db.Integer, unique=True, nullable=False)
     country = db.Column(db.String(120), nullable=False)
     curr_degree = db.Column(db.String(120), nullable=False)
     year_of_study = db.Column(db.Integer, nullable=False)
     credits = db.Column(db.Integer, nullable=False)
     enrollment = db.Column(db.String(120), nullable=False)
-    transcript = db.Column(db.LargeBinary)
-    resume = db.Column(db.LargeBinary)
-    essay = db.Column(db.LargeBinary)
+
+    transcript = db.Column(db.String(120), nullable=False)
+    resume = db.Column(db.String(120), nullable=False)
+    essay = db.Column(db.String(120), nullable=False)
 
     def toDict(self):
         return {
             "Student ID": self.studentID,
             "Name": self.name,
             "Email": self.email,
-            "UWI ID": self.UWI_ID,
+            "uwiid": self.uwiid,
             "Country": self.country,
             "Current Degree": self.curr_degree,
             "Year of Study": self.year_of_study,
@@ -67,4 +67,54 @@ class Student(db.Model, UserMixin, ):
             "Transcript": self.transcript,
             "Resume": self.resume,
             "Essay": self.essay
+        }
+
+
+class FileContents(db.Model):
+    files_id = db.Column(db.Integer, primary_key=True)
+    transcript = db.Column(db.LargeBinary(), nullable=False)
+    resume = db.Column(db.LargeBinary(), nullable=False)
+    essay = db.Column(db.LargeBinary(), nullable=False)
+    student_uwiid = db.Column(db.Integer, db.ForeignKey('student.uwiid'), nullable=False)
+
+
+class Business(db.Model, UserMixin, ):
+    businessID = db.Column(db.Integer, primary_key=True)
+    bname = db.Column(db.String(120), nullable=False)
+    num_interns = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    credits = db.Column(db.Integer, nullable=False)
+    stipend = db.Column(db.Integer, nullable=False)
+    tech_skills = db.Column(db.String(300), nullable=False)
+    soft_skills = db.Column(db.String(300), nullable=False)
+
+    def toDict(self):
+        return {
+            "Business ID": self.businessID,
+            "Name": self.bname,
+            "Number of Interns": self.num_interns,
+            "Duration": self.duration,
+            "credits": self.credits,
+            "stipend": self.stipend,
+            "Technical Skills": self.tech_skills,
+            "Soft Skills": self.soft_skills
+
+        }
+
+
+class Internship(db.Model, UserMixin, ):
+    internshipID = db.Column(db.Integer, primary_key=True)
+    proj_name = db.Column(db.String(120), nullable=False)
+    proj_descript = db.Column(db.String(300), nullable=False)
+    activities = db.Column(db.String(300), nullable=False)
+    business_ID = db.Column(db.Integer, db.ForeignKey('business.businessID'), nullable=False)
+
+    def toDict(self):
+        return {
+            "Internship ID": self.businessID,
+            "Project Name": self.proj_name,
+            "Project Description": self.proj_descript,
+            "Activities": self.activities,
+            "Business ID": self.business_ID
+
         }
