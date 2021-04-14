68a13e00710470f765ec9f356c94d3a4746b0966
RandyDeo <54963299+RandyDeo@users.noreply.github.com>
2021-04-14 16:39:47 -0400

    Add files via upload

---
+++ test_MatchMyInterns.py
@@ -0,0 +1,55 @@
+import unittest
+from unittest import TestCase
+from main import db, User, Student, Business, Internship, Report, app
+
+
+class MatchMyInternsTest(unittest.TestCase):
+
+    def test_valid_login(self):
+        test = app.test_client(self)
+        response = test.post(
+            '/login',
+            data=dict(email='randydeo@gmail.com', passw='password'),
+            follow_redirects=True
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_wrong_email(self):
+        test = app.test_client(self)
+        response = test.post(
+            '/login',
+            data=dict(email='rdydeo@gmail.com', passw='password'),
+            follow_redirects=True
+        )
+        self.assertIn(b'Invalid Email, Please create an account!',
+                      response.data)
+
+    def test_wrong_password(self):
+        test = app.test_client(self)
+        response = test.post(
+            '/login',
+            data=dict(email='randydeo@gmail.com', passw='passw'),
+            follow_redirects=True
+        )
+        self.assertIn(b'Invalid Password!',
+                      response.data)
+
+#It saves the email in db, so if you run twice with same email, it will fail. PUT NEW EMAIL FOR EACH RUN
+    def test_valid_signup(self):
+        test = app.test_client(self)
+        response = test.post(
+            '/signup',
+            data=dict(email='7@gmail.com', name="John Doe", occupation="Student", passw='password'),
+            follow_redirects=True
+        )
+        self.assertEqual(response.status_code, 201)
+
+    def test_email_already_exists_signup(self):
+        test = app.test_client(self)
+        response = test.post(
+            '/signup',
+            data=dict(email='randydeo@gmail.com', name="John Doe", occupation="Student", passw='password'),
+            follow_redirects=True
+        )
+        self.assertEqual(response.status_code, 401)
+
